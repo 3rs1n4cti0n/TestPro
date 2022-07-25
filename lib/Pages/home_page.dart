bf06c16370e6049dca6ca75d37ff2d4b28e2624d
3rs1n4cti0n <hatipersan@gmail.com>
2022-07-25 08:06:20 +0300

    made so when the user presses back button on home page it redirects to landing page.

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -9,161 +9,170 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    gradient: LinearGradient(
-                      colors: [Colors.indigo[200]!, Colors.indigo[900]!],
-                      end: Alignment.topLeft,
-                      begin: Alignment.bottomRight,
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          blurRadius: 5,
-                          spreadRadius: 3),
-                    ],
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(10.0),
-                      bottomRight: Radius.circular(10.0),
-                    )),
-                width: double.infinity,
-                height: 250,
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.account_circle,
-                      size: 180,
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                "Welcome to TextPRO,\n${FirebaseAuth.instance.currentUser!.isAnonymous ? "Anonymous" : FitnessUser.name}!",
-                                overflow: TextOverflow.clip,
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
+    return WillPopScope(
+      onWillPop: () async {
+        await FirebaseAuth.instance.signOut().then((value) =>
+            Navigator.of(context).popUntil((route) => route.isFirst));
+
+        return true;
+      },
+      child: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      gradient: LinearGradient(
+                        colors: [Colors.indigo[200]!, Colors.indigo[900]!],
+                        end: Alignment.topLeft,
+                        begin: Alignment.bottomRight,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 5,
+                            spreadRadius: 3),
+                      ],
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0),
+                      )),
+                  width: double.infinity,
+                  height: 250,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.account_circle,
+                        size: 180,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  "Welcome to TextPRO,\n${FirebaseAuth.instance.currentUser!.isAnonymous ? "Anonymous" : FitnessUser.name}!",
+                                  overflow: TextOverflow.clip,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      decoration: TextDecoration.none),
+                                ),
+                              ),
+                              Text(
+                                FirebaseAuth.instance.currentUser!.isAnonymous
+                                    ? "Unknown"
+                                    : FitnessUser.gender
+                                        ? "male"
+                                        : "female",
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    color: Colors.orange[800],
                                     decoration: TextDecoration.none),
                               ),
-                            ),
-                            Text(
-                              FirebaseAuth.instance.currentUser!.isAnonymous
-                                  ? "Unknown"
-                                  : FitnessUser.gender
-                                      ? "male"
-                                      : "female",
-                              style: TextStyle(
-                                  fontSize: 32,
-                                  color: Colors.orange[800],
-                                  decoration: TextDecoration.none),
-                            ),
-                            const Expanded(child: SizedBox()),
-                            Row(
-                              children: [
-                                const Expanded(child: SizedBox()),
-                                TextButton(
-                                    onPressed: () {
-                                      FirebaseAuth.instance.signOut();
-                                      Navigator.pop(context);
-                                    },
-                                    child: Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: Text(
-                                          "Sign out",
-                                          style: TextStyle(
-                                              color: Colors.purple[900]),
-                                        ))),
-                              ],
-                            )
-                          ],
+                              const Expanded(child: SizedBox()),
+                              Row(
+                                children: [
+                                  const Expanded(child: SizedBox()),
+                                  TextButton(
+                                      onPressed: () {
+                                        FirebaseAuth.instance.signOut();
+                                        Navigator.pop(context);
+                                      },
+                                      child: Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: Text(
+                                            "Sign out",
+                                            style: TextStyle(
+                                                color: Colors.purple[900]),
+                                          ))),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
+                    ],
+                  )),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    width: double.infinity,
+                    height: 34,
+                    decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                              color: Colors.black.withOpacity(0.5),
+                              width: 0.5)),
                     ),
-                  ],
-                )),
-            Flexible(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Container(
-                  width: double.infinity,
-                  height: 34,
-                  decoration: BoxDecoration(
-                    border: Border(
-                        bottom: BorderSide(
-                            color: Colors.black.withOpacity(0.5), width: 0.5)),
-                  ),
-                  child: const Align(
-                    alignment: Alignment.topCenter,
-                    child: Text(
-                      "Information",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w100,
-                          decoration: TextDecoration.none),
+                    child: const Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        "Information",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w100,
+                            decoration: TextDecoration.none),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: SizedBox(
-                height: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "age: ${FirebaseAuth.instance.currentUser!.isAnonymous ? "Unknown" : FitnessUser.age}",
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w100,
-                          decoration: TextDecoration.none),
-                    ),
-                    Divider(
-                      thickness: 1,
-                      color: Colors.black.withOpacity(0.4),
-                    ),
-                    Text(
-                      "height: ${FirebaseAuth.instance.currentUser!.isAnonymous ? "Unknown" : FitnessUser.height}",
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w100,
-                          decoration: TextDecoration.none),
-                    ),
-                    Divider(
-                      thickness: 1,
-                      color: Colors.black.withOpacity(0.4),
-                    ),
-                    Text(
-                      "weight: ${FirebaseAuth.instance.currentUser!.isAnonymous ? "Unknown" : FitnessUser.weight}",
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w100,
-                          decoration: TextDecoration.none),
-                    ),
-                    Divider(
-                      thickness: 1,
-                      color: Colors.black.withOpacity(0.4),
-                    ),
-                  ],
+              Expanded(
+                child: SizedBox(
+                  height: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "age: ${FirebaseAuth.instance.currentUser!.isAnonymous ? "Unknown" : FitnessUser.age}",
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w100,
+                            decoration: TextDecoration.none),
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                      Text(
+                        "height: ${FirebaseAuth.instance.currentUser!.isAnonymous ? "Unknown" : FitnessUser.height}",
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w100,
+                            decoration: TextDecoration.none),
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                      Text(
+                        "weight: ${FirebaseAuth.instance.currentUser!.isAnonymous ? "Unknown" : FitnessUser.weight}",
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w100,
+                            decoration: TextDecoration.none),
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
